af9e649e9dda540b197f086f8c8301d13e8da457
Zasilus <asg101@case.edu>
2018-06-29 15:24:54 -0400

    1. Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.17;
+
+contract Election {
+	//Store candidate
+	//Read candidate
+	string public candidate;
+	//Constructor
+	function Election () public {
+		candidate = "Candidate 1";
+		
+	}
+
+}
